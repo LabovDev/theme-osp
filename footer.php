fe369393fde41886f21c0ba357798f54e1013320
Josh Tuck <josh@rustbelt.co>
2018-12-20 16:14:27 -0500

    Josh makes some proofing changes.
    In this episode, Josh makes some proofing changes.

--- footer.php
+++ footer.php
@@ -15,7 +15,7 @@
 				<div class="ftr__legal">
 					<p class="txt txt--mouse"><?php echo sprintf( __( '%1$s %2$s %3$s. All Rights Reserved.', 'blankslate' ), '&copy;', date( 'Y' ), esc_html( get_bloginfo( 'name' ) ) ); ?></p>
 					<p class="txt txt--mouse">
-						<a class="link link--mouse" href="/?page_id=32">A Commercial Specialty Truck Holdings Company</a>
+						<a class="link link--mouse" href="<?php bloginfo('url'); ?>/?page_id=32">A Commercial Specialty Truck Holdings Company</a>
 					</p>
 				</div>
 			</footer>
