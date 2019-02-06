7f3a2947a7bd406cab0385863c4a5a5f2d8176c1
Josh Tuck <josh@rustbelt.co>
2019-02-06 11:17:34 -0500

    Josh makes minor changes.
    In this episode, Josh makes minor changes.

--- header.php
+++ header.php
@@ -11,6 +11,15 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/theme-osp/js/doubletaptogo.min.js"></script>
 	<script type="text/javascript" src="<?php bloginfo('url'); ?>/wp-content/themes/theme-osp/js/csth-scripts-min.js"></script>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=UA-63170690-10"></script>
+	<script>
+	  window.dataLayer = window.dataLayer || [];
+	  function gtag(){dataLayer.push(arguments);}
+	  gtag('js', new Date());
+	
+	  gtag('config', 'UA-63170690-10');
+	</script>
 </head>
 
 <body <?php body_class(); ?>>
--- page-csth.php
+++ page-csth.php
@@ -132,8 +132,8 @@ get_header(); ?>
 		</section> <!-- END .blocFeatSplit -->
 
 		<section class="wrap blocCta">
-			<a href="http://www.ezpacktrucks.com/" target="_blank" class="btn btn--prd btn--dark">Vist E-Z Pack Website</a>
-			<a href="https://www.continentalmixers.com/" target="_blank" class="btn btn--prd btn--dark">Vist Continental Mixers Website</a>
+			<a href="http://www.ezpacktrucks.com/" target="_blank" class="btn btn--prd btn--dark">Visit E-Z Pack Website</a>
+			<a href="http://www.continentalmixers.com/" target="_blank" class="btn btn--prd btn--dark">Visit Continental Mixers Website</a>
 		</section> <!-- END .blocCta -->
 			
 		</div> <!-- END .wrap--mxw -->
